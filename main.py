ad225e11e2aca750da89c277baa271ba99fa2ccc
Ahmed <noxioustab@gmail.com>
2024-12-02 19:01:53 +0530

    Added fly.toml

--- main.py
+++ main.py
@@ -92,4 +92,4 @@ def submit_code():
     return jsonify({"verdict": "Accepted"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
