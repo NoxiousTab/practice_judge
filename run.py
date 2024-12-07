e877c8bd54a7b825bc7a7d317321ca00816bb3dc
Ahmed Tabish <noxioustab@gmail.com>
2024-12-07 13:29:00 +0530

    Deployment Configurations

--- run.py
+++ run.py
@@ -15,5 +15,4 @@ with app.app_context():
 	db.create_all()
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
